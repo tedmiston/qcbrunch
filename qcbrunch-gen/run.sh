3d0b7058772bd09463b49ea7862b7bdb9480f306
Taylor D. Edmiston <tedmiston@gmail.com>
2018-11-11 22:48:19 -0500

    Simplify qcbrunch-gen script

--- qcbrunch-gen/run.sh
+++ qcbrunch-gen/run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# cookiecutter place-cc
 cookiecutter place-cc --output-dir=output
-# cookiecutter place-cc --output-dir=output --config-file=data.yaml
-# cookiecutter place-cc --config-file=data.yaml
-# cookiecutter place-cc --output-dir=output --config-file=data.yaml && cat output/**/*.html
