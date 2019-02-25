867e2443f9459e636bab7d74ddbc1aaafb25b82b
Taylor D. Edmiston <tedmiston@gmail.com>
2019-02-25 00:42:03 -0500

    Store and read Google Maps count from file

--- docker/google-maps-email/app.sh
+++ docker/google-maps-email/app.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo TODO
+# TODO: REPO_COUNT check
+
+GOOGLE_MAPS_COUNT=$(cat google_maps_views.txt)
+echo GOOGLE_MAPS_COUNT=${GOOGLE_MAPS_COUNT}
+
+# TODO: email on diff
--- docker/google-maps-views/app.sh
+++ docker/google-maps-views/app.sh
@@ -7,3 +7,5 @@ OUTPUT_FILE='out.html'
 curl --silent --output ${OUTPUT_FILE} ${MAP_URL}
 VIEW_COUNT=$(cat ${OUTPUT_FILE} | grep _pageData | sed -E 's/.*,([[:digit:]]{4,}).*,true,true.*/\1/')
 echo ${VIEW_COUNT} views
+echo ${VIEW_COUNT} > google_maps_views.txt
+cat google_maps_views.txt
