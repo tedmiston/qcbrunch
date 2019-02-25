92bf78b9ba75edaad055eb3817555230993c9294
Taylor D. Edmiston <tedmiston@gmail.com>
2019-02-25 00:02:32 -0500

    Add Google Maps Views step

---
+++ docker/google-maps-views/app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+MAP_URL='https://www.google.com/maps/d/viewer?mid=1q1ikNFAU1WKUtkeGjK2mTaq6dvo&usp=sharing'
+OUTPUT_FILE='out.html'
+
+curl --silent --output ${OUTPUT_FILE} ${MAP_URL}
+VIEW_COUNT=$(cat ${OUTPUT_FILE} | grep _pageData | sed -E 's/.*,([[:digit:]]{4,}).*,true,true.*/\1/')
+echo ${VIEW_COUNT} views
