d078e12205ba59877526bcc23e4d4f341a06f0c4
Taylor D. Edmiston <tedmiston@gmail.com>
2019-05-26 15:47:36 -0400

    Add full lifecycle `dev run` command

--- qcbrunch-cli/qcbrunchcli/commands/dev.py
+++ qcbrunch-cli/qcbrunchcli/commands/dev.py
@@ -2,7 +2,7 @@ import subprocess
 
 import click
 
-from ..conf import BUILD_DIR
+from ..conf import BUILD_DIR, SOURCE_DIR
 
 @click.group()
 def dev():
@@ -11,3 +11,7 @@ def dev():
 @dev.command()
 def serve():
     subprocess.run('serve -l 5001', cwd=BUILD_DIR, shell=True)
+
+@dev.command()
+def run():
+    subprocess.run('qcbrunch clean && qcbrunch render && qcbrunch build && qcbrunch dev serve', cwd=SOURCE_DIR, shell=True)
