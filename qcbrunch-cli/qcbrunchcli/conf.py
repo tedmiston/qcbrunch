07a2dabacafc275e8cd49be6673e79f36ace2d29
Taylor D. Edmiston <tedmiston@gmail.com>
2019-05-26 14:39:17 -0400

    Add support for env vars in PROJECT_ROOT

--- qcbrunch-cli/qcbrunchcli/conf.py
+++ qcbrunch-cli/qcbrunchcli/conf.py
@@ -3,7 +3,8 @@ import os.path
 
 # fail fast if a required env var is not provided
 
-PROJECT_ROOT = os.path.expanduser(os.environ['QCBRUNCH_ROOT'])
+PROJECT_ROOT = os.path.expanduser(os.path.expandvars(os.environ['QCBRUNCH_ROOT']))
+print('PROJECT_ROOT=', PROJECT_ROOT)
 
 SOURCE_DIR = PROJECT_ROOT
 
