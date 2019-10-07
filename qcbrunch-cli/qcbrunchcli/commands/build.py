57cf3053e33ed60959ad0014c86b6abbe3b1bb0b
Taylor D. Edmiston <tedmiston@gmail.com>
2019-10-06 22:03:56 -0400

    Remove Serve config file

--- qcbrunch-cli/qcbrunchcli/commands/build.py
+++ qcbrunch-cli/qcbrunchcli/commands/build.py
@@ -7,5 +7,5 @@ from ..conf import SOURCE_DIR
 @click.command()
 def build():
     directories = 'css images js'
-    files = '.nowignore browserconfig.xml now.json robots.txt serve.json sitemap.txt stats.html'
+    files = '.nowignore browserconfig.xml now.json robots.txt sitemap.txt stats.html'
     subprocess.run(f'cp -R {directories} {files} build/', cwd=SOURCE_DIR, shell=True)
