ce251447d89f5ced2f19188c569d7dc284aa9289
Taylor D. Edmiston <tedmiston@gmail.com>
2019-10-13 20:44:02 -0400

    Fix Yelp Closed bug (#76)

--- docker/yelp-closed-detector/yelp_collection_parser.py
+++ docker/yelp-closed-detector/yelp_collection_parser.py
@@ -1,5 +1,7 @@
 from requests_html import HTML, HTMLSession
 
+session = None
+
 def place_serializer(biz):
     return {'name': biz.text, 'url': list(biz.absolute_links)[0]}
 
@@ -11,18 +13,20 @@ def get_page_urls(initial_offset, total_count, page_size):
 def get_places_from_page(url):
     """Extract places from pages 2 and beyond."""
     print(f'* fetching page {url}')
-    html = HTML(html=HTMLSession().get(url).json()['list_markup'])
+    html = HTML(html=session.get(url).json()['list_markup'], url='https://www.yelp.com/')
     return [place_serializer(biz) for biz in html.find('a.biz-name')]
 
 def get_initial_yelp_collection_places():
     """Get places from page 1."""
+    global session
     print('* fetching first page')
-    response = HTMLSession().get('https://www.yelp.com/collection/Ntw8wQeFY35dpevGB-Et_A?sort_by=alpha')
+    session = HTMLSession()
+    response = session.get('https://www.yelp.com/collection/Ntw8wQeFY35dpevGB-Et_A?sort_by=alpha')
     return [place_serializer(biz) for biz in response.html.find('a.biz-name')]
 
 def get_all_yelp_place_urls():
     all_places = get_initial_yelp_collection_places()
     # TODO: pull total count dynamically
-    for url in get_page_urls(initial_offset=30, total_count=129, page_size=30):
+    for url in get_page_urls(initial_offset=30, total_count=142, page_size=30):
         all_places.extend(get_places_from_page(url))
     return [x['url'] for x in all_places]
