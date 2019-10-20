f3d9e0878596faa26d3ae9840b14ee7f1f387347
Taylor D. Edmiston <tedmiston@gmail.com>
2019-10-20 08:45:30 -0400

    Copy dynamically generated Now configs

--- qcbrunch-cli/qcbrunchcli/commands/build.py
+++ qcbrunch-cli/qcbrunchcli/commands/build.py
@@ -6,6 +6,6 @@ from ..conf import SOURCE_DIR
 
 @click.command()
 def build():
-    directories = 'css images js'
-    files = '.nowignore browserconfig.xml now.dev.json now.json robots.txt sitemap.txt stats.html'
+    directories = '.now css images js'
+    files = '.nowignore browserconfig.xml robots.txt sitemap.txt stats.html'
     subprocess.run(f'cp -R {directories} {files} build/', cwd=SOURCE_DIR, shell=True)
