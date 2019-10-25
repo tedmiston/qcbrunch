9ce63a6e66cb9e687333afda57df87ee65248a88
Taylor D. Edmiston <tedmiston@gmail.com>
2019-10-25 19:07:05 -0400

    Add entrypoint

---
+++ .github/actions/healthchecks/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -euo pipefail
+
+if [ $# -eq 0 ]; then
+  echo 'Usage: <job status> <healthchecks url>'
+  exit 1
+fi
+
+job_status="${1}"
+url="${2}"
+
+if [ "${job_status}" == 'success' ];
+then
+  url="${url}"
+elif [ "${job_status}" == 'failure' ];
+then
+  url="${url}"/fail
+elif [ "${job_status}" == 'cancelled' ];
+then
+  echo 'job cancelled'
+  exit 1
+else
+  echo 'invalid job status'
+  exit 1
+fi
+
+echo curl --retry 3 "${url}"
