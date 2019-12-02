7277a05c995e4958ddcd2663147f25f7f183c8f6
Taylor D. Edmiston <tedmiston@users.noreply.github.com>
2019-12-02 07:36:12 -0500

    Bump updated date

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ function checkWhenLastUpdated() {
   // FIXME: getting aggressively rate limited
   // TODO: pull from gh-pages branch
 
-  $('#updated-date').text('November 2019');
+  $('#updated-date').text('December 2019');
 
   // var updatedDate;
   // $.getJSON("https://api.github.com/repos/tedmiston/qcbrunch/commits/HEAD")
