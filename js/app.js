c3c211caf69544f3437be091c684fd459f62ce17
Taylor D. Edmiston <tedmiston@gmail.com>
2020-02-05 05:47:19 +0000

    Automate updated date

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ function checkWhenLastUpdated() {
   // FIXME: getting aggressively rate limited
   // TODO: pull from gh-pages branch
 
-  $('#updated-date').text('January 2020');
+//   $('#updated-date').text('January 2020');
 
   // var updatedDate;
   // $.getJSON("https://api.github.com/repos/tedmiston/qcbrunch/commits/HEAD")
