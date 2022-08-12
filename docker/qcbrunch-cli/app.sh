415f7c6abcb798575c997530d9c0e380d384e2e1
Taylor Edmiston <tedmiston@gmail.com>
2022-08-12 11:03:57 -0400

    Try calling pip3 directly

--- docker/qcbrunch-cli/app.sh
+++ docker/qcbrunch-cli/app.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 set -Eeuo pipefail
 
 export PATH="/github/home/.local/bin:${PATH}"
 
 apt-get update && apt-get install git --yes
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --editable qcbrunch-cli
+pip3 install --upgrade pip
+pip3 install --editable qcbrunch-cli
 
 qcbrunch clean
 qcbrunch render
