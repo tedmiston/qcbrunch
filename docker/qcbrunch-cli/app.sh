94cfe7ce27b350affd79f505d93c46a240c41a3f
Taylor Edmiston <tedmiston@gmail.com>
2022-08-12 10:54:13 -0400

    Re-enable latest Pip

--- docker/qcbrunch-cli/app.sh
+++ docker/qcbrunch-cli/app.sh
@@ -5,7 +5,7 @@ export PATH="/github/home/.local/bin:${PATH}"
 
 apt-get update && apt-get install git --yes
 
-# python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 python3 -m pip install --editable qcbrunch-cli
 
 qcbrunch clean
