a1b169e7b01f0434ea25eff7916906f56f3916ac
Taylor D. Edmiston <tedmiston@gmail.com>
2022-08-17 15:11:02 -0400

    Copy vercel.json to build dir

--- qcbrunch-cli/qcbrunchcli/commands/build.py
+++ qcbrunch-cli/qcbrunchcli/commands/build.py
@@ -7,5 +7,5 @@ from ..conf import SOURCE_DIR
 @click.command()
 def build():
     directories = '.vercel css images js'
-    files = '.vercelignore browserconfig.xml robots.txt sitemap.txt stats.html'
+    files = '.vercelignore browserconfig.xml robots.txt sitemap.txt stats.html vercel.json'
     subprocess.run(f'cp -R {directories} {files} build/', cwd=SOURCE_DIR, shell=True)
